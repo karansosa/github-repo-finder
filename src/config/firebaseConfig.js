1e18379a29ee5db45017362bcff448f89f156bf3
Karan Sosa <karansosa1995@gmail.com>
2020-10-07 12:40:59 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default  {
+    apiKey: "AIzaSyDn9Oa5ZOiPfvbY68rxjfsgigBzogUw8Jw",
+    authDomain: "github-repository-7d0a2.firebaseapp.com",
+    databaseURL: "https://github-repository-7d0a2.firebaseio.com",
+    projectId: "github-repository-7d0a2",
+    storageBucket: "github-repository-7d0a2.appspot.com",
+    messagingSenderId: "651120524071",
+    appId: "1:651120524071:web:38b35d1a2e7339f90b8702",
+    measurementId: "G-RBHNJ730PB"
+  };
